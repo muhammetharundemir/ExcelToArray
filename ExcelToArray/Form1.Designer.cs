73dab38052c1215150676c3284e5f647b2f00d40
muhamammetharundemir <muhamammetharundemir@gmail.com>
2021-10-14 00:21:39 +0300

    Add project files.

---
+++ ExcelToArray/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace ExcelToArray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonExcel2Text = new System.Windows.Forms.Button();
+            this.buttonSelectFile = new System.Windows.Forms.Button();
+            this.labelConversionCompleted = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonMinimized = new System.Windows.Forms.Button();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.panelAbove = new System.Windows.Forms.Panel();
+            this.buttonCSharp = new System.Windows.Forms.Button();
+            this.buttonCpp = new System.Windows.Forms.Button();
+            this.richTextBoxArray = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelAbove.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonExcel2Text
+            // 
+            this.buttonExcel2Text.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExcel2Text.ForeColor = System.Drawing.Color.LightGray;
+            this.buttonExcel2Text.Location = new System.Drawing.Point(125, 40);
+            this.buttonExcel2Text.Name = "buttonExcel2Text";
+            this.buttonExcel2Text.Size = new System.Drawing.Size(65, 50);
+            this.buttonExcel2Text.TabIndex = 0;
+            this.buttonExcel2Text.TabStop = false;
+            this.buttonExcel2Text.Text = "Excel to Text";
+            this.buttonExcel2Text.UseVisualStyleBackColor = true;
+            this.buttonExcel2Text.Click += new System.EventHandler(this.buttonExcel2Text_Click);
+            // 
+            // buttonSelectFile
+            // 
+            this.buttonSelectFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSelectFile.ForeColor = System.Drawing.Color.LightGray;
+            this.buttonSelectFile.Location = new System.Drawing.Point(10, 40);
+            this.buttonSelectFile.Name = "buttonSelectFile";
+            this.buttonSelectFile.Size = new System.Drawing.Size(65, 50);
+            this.buttonSelectFile.TabIndex = 2;
+            this.buttonSelectFile.TabStop = false;
+            this.buttonSelectFile.Text = "Select File";
+            this.buttonSelectFile.UseVisualStyleBackColor = true;
+            this.buttonSelectFile.Click += new System.EventHandler(this.buttonSelectFile_Click);
+            // 
+            // labelConversionCompleted
+            // 
+            this.labelConversionCompleted.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelConversionCompleted.AutoSize = true;
+            this.labelConversionCompleted.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelConversionCompleted.ForeColor = System.Drawing.Color.DarkSeaGreen;
+            this.labelConversionCompleted.Location = new System.Drawing.Point(54, 93);
+            this.labelConversionCompleted.Name = "labelConversionCompleted";
+            this.labelConversionCompleted.Size = new System.Drawing.Size(90, 17);
+            this.labelConversionCompleted.TabIndex = 3;
+            this.labelConversionCompleted.Text = "Processing...";
+            this.labelConversionCompleted.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelConversionCompleted.Visible = false;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.buttonExit.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.buttonExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(150)))));
+            this.buttonExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExit.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonExit.ForeColor = System.Drawing.Color.LightGray;
+            this.buttonExit.Location = new System.Drawing.Point(164, -2);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(39, 33);
+            this.buttonExit.TabIndex = 307;
+            this.buttonExit.TabStop = false;
+            this.buttonExit.Text = "x";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonMinimized
+            // 
+            this.buttonMinimized.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.buttonMinimized.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.buttonMinimized.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(150)))));
+            this.buttonMinimized.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonMinimized.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMinimized.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonMinimized.ForeColor = System.Drawing.Color.LightGray;
+            this.buttonMinimized.Location = new System.Drawing.Point(126, -8);
+            this.buttonMinimized.Name = "buttonMinimized";
+            this.buttonMinimized.Size = new System.Drawing.Size(38, 39);
+            this.buttonMinimized.TabIndex = 308;
+            this.buttonMinimized.TabStop = false;
+            this.buttonMinimized.Text = "_";
+            this.buttonMinimized.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonMinimized.UseVisualStyleBackColor = false;
+            this.buttonMinimized.Click += new System.EventHandler(this.buttonMinimized_Click);
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.labelTitle.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold);
+            this.labelTitle.ForeColor = System.Drawing.Color.LightGray;
+            this.labelTitle.Location = new System.Drawing.Point(12, 10);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(38, 15);
+            this.labelTitle.TabIndex = 309;
+            this.labelTitle.Text = "Array";
+            this.labelTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.labelTitle_MouseDown);
+            this.labelTitle.MouseMove += new System.Windows.Forms.MouseEventHandler(this.labelTitle_MouseMove);
+            this.labelTitle.MouseUp += new System.Windows.Forms.MouseEventHandler(this.labelTitle_MouseUp);
+            // 
+            // panelAbove
+            // 
+            this.panelAbove.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.panelAbove.Controls.Add(this.label1);
+            this.panelAbove.Controls.Add(this.buttonExit);
+            this.panelAbove.Controls.Add(this.labelTitle);
+            this.panelAbove.Controls.Add(this.buttonMinimized);
+            this.panelAbove.Location = new System.Drawing.Point(-1, -1);
+            this.panelAbove.Name = "panelAbove";
+            this.panelAbove.Size = new System.Drawing.Size(202, 32);
+            this.panelAbove.TabIndex = 310;
+            this.panelAbove.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelAbove_MouseDown);
+            this.panelAbove.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelAbove_MouseMove);
+            this.panelAbove.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelAbove_MouseUp);
+            // 
+            // buttonCSharp
+            // 
+            this.buttonCSharp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCSharp.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCSharp.ForeColor = System.Drawing.Color.LightGray;
+            this.buttonCSharp.Location = new System.Drawing.Point(80, 40);
+            this.buttonCSharp.Name = "buttonCSharp";
+            this.buttonCSharp.Size = new System.Drawing.Size(40, 24);
+            this.buttonCSharp.TabIndex = 311;
+            this.buttonCSharp.TabStop = false;
+            this.buttonCSharp.Text = "C#";
+            this.buttonCSharp.UseVisualStyleBackColor = true;
+            this.buttonCSharp.Click += new System.EventHandler(this.buttonCSharp_Click);
+            // 
+            // buttonCpp
+            // 
+            this.buttonCpp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCpp.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCpp.ForeColor = System.Drawing.Color.LightGray;
+            this.buttonCpp.Location = new System.Drawing.Point(80, 66);
+            this.buttonCpp.Name = "buttonCpp";
+            this.buttonCpp.Size = new System.Drawing.Size(40, 24);
+            this.buttonCpp.TabIndex = 312;
+            this.buttonCpp.TabStop = false;
+            this.buttonCpp.Text = "C++";
+            this.buttonCpp.UseVisualStyleBackColor = true;
+            this.buttonCpp.Click += new System.EventHandler(this.buttonCpp_Click);
+            // 
+            // richTextBoxArray
+            // 
+            this.richTextBoxArray.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.richTextBoxArray.Location = new System.Drawing.Point(400, 100);
+            this.richTextBoxArray.Name = "richTextBoxArray";
+            this.richTextBoxArray.ReadOnly = true;
+            this.richTextBoxArray.Size = new System.Drawing.Size(1, 1);
+            this.richTextBoxArray.TabIndex = 313;
+            this.richTextBoxArray.Text = "";
+            this.richTextBoxArray.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Arial", 7.5F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(47, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 12);
+            this.label1.TabIndex = 310;
+            this.label1.Text = "Excel to Text";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(200, 115);
+            this.Controls.Add(this.richTextBoxArray);
+            this.Controls.Add(this.buttonCpp);
+            this.Controls.Add(this.buttonCSharp);
+            this.Controls.Add(this.panelAbove);
+            this.Controls.Add(this.labelConversionCompleted);
+            this.Controls.Add(this.buttonSelectFile);
+            this.Controls.Add(this.buttonExcel2Text);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Excel to Text";
+            this.panelAbove.ResumeLayout(false);
+            this.panelAbove.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonExcel2Text;
+        private System.Windows.Forms.Button buttonSelectFile;
+        private System.Windows.Forms.Label labelConversionCompleted;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonMinimized;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Panel panelAbove;
+        private System.Windows.Forms.Button buttonCSharp;
+        private System.Windows.Forms.Button buttonCpp;
+        private System.Windows.Forms.RichTextBox richTextBoxArray;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ExcelToArray/Form1.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+
+namespace ExcelToArray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //
+        //
+        //
+        string DosyaYolu;
+        string DosyaAdi;
+        //
+        StreamWriter file_writer;
+		//StreamReader file_reader;
+        //
+        string path = "";
+        _Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+        //
+        //
+        //
+        bool cSharp = false;
+        bool cpp = false;
+        bool excelToArray = false;
+        bool cSharpWrite = false;
+        bool cppWrite = false;
+        //
+        private void buttonSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Excel Dosyası | *.xls; *.xlsx; *.xlsm";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                DosyaYolu = file.FileName;// seçilen dosyanın tüm yolunu verir
+                DosyaAdi = file.SafeFileName;// seçilen dosyanın adını verir.
+                labelConversionCompleted.Visible = false;
+                buttonSelectFile.ForeColor = Color.DarkSeaGreen;
+                cpp = true;
+                cSharp = true;
+            }
+            else
+            {
+                buttonSelectFile.ForeColor = Color.LightGray;
+                cpp = false;
+                cSharp = false;
+                excelToArray = false;
+                //MessageBox.Show("Dosya Seçilmedi.");
+            }
+        }
+        //
+        //
+        //
+        private void buttonExcel2Text_Click(object sender, EventArgs e)
+        {
+            if (excelToArray)
+            {
+                labelConversionCompleted.Text = "Processing...";
+                labelConversionCompleted.Location= new System.Drawing.Point(54, 93);
+                labelConversionCompleted.Visible = true;
+                try
+                {
+                    if (cSharpWrite)
+                    {
+                        OpenFileCSharp(DosyaYolu);
+                    }
+                    else if (cppWrite)
+                    {
+                        OpenFileCpp(DosyaYolu);
+                    }
+                    buttonExcel2Text.ForeColor = Color.DarkSeaGreen;
+                    excelToArray = false;
+                }
+                catch
+                {
+                    labelConversionCompleted.Visible = false;
+                }
+            }
+        }
+        //
+        //
+        //
+        public void OpenFileCSharp(string location)
+        {
+            string address = DosyaYolu + ".txt";
+            Excel(location, 1);
+            for (int j = 1; j < 10000; j++)
+            {
+                for (int i = 1; i < 10000; i++)
+                {
+                    if (ws.Cells[i, j].Value2 != null)
+                    {
+                        if (i == 1)
+                        {
+                            //richTextBox1.Text += ws.Cells[i, j].Value2 + " = { ";
+                            file_writer = new StreamWriter(address, true);
+                            file_writer.Write("double[] " + ws.Cells[i, j].Value2 + " = { ");
+                            file_writer.Close();
+                        }
+                        else if (i == 2)
+                        {
+                            //richTextBox1.Text += ws.Cells[i, j].Value2;
+                            file_writer = new StreamWriter(address, true);
+                            file_writer.Write(ws.Cells[i, j].Value2);
+                            file_writer.Close();
+                        }
+                        else
+                        {
+                            //richTextBox1.Text += ", " + ws.Cells[i, j].Value2;
+                            file_writer = new StreamWriter(address, true);
+                            file_writer.Write(", " + ws.Cells[i, j].Value2);
+                            file_writer.Close();
+                        }
+                    }
+                    else if (ws.Cells[1, j + 1].Value2 != null)
+                    {
+                        //richTextBox1.Text += " };\n";
+                        file_writer = new StreamWriter(address, true);
+                        file_writer.Write(" };\n");
+                        file_writer.Close();
+                        i = 10000;
+                    }
+                    else
+                    {
+                        //richTextBox1.Text += " };\n";
+                        file_writer = new StreamWriter(address, true);
+                        file_writer.Write(" };\n");
+                        file_writer.Close();
+                        labelConversionCompleted.Text = "Conversion Completed";
+                        labelConversionCompleted.Location = new System.Drawing.Point(29, 93);
+                        labelConversionCompleted.Visible = true;
+                        i = 10000;
+                        j = 10000;
+                    }         
+                }
+            }
+            Excel("0", 0);
+        }
+        //
+        //
+        //
+        public void OpenFileCpp(string location)
+        {
+            string arrayName="";
+            string address = DosyaYolu + ".txt";
+            Excel(location, 1);
+            for (int j = 1; j < 10000; j++)
+            {
+                for (int i = 1; i < 10000; i++)
+                {
+                    if (ws.Cells[i, j].Value2 != null)
+                    {
+                        if (i == 1)
+                        {
+                            //richTextBox1.Text += ws.Cells[i, j].Value2 + " = { ";
+                            //file_writer.Write(ws.Cells[i, j].Value2 + " = { ");
+                            //file_writer.Write("{ ");
+                            //file_writer.Close();
+                            arrayName = Convert.ToString(ws.Cells[i, j].Value2);
+                            richTextBoxArray.Text = "{ ";
+                        }
+                        else if (i == 2)
+                        {
+                            //richTextBox1.Text += ws.Cells[i, j].Value2;
+                            //file_writer = new StreamWriter(address, true);
+                            //file_writer.Write(ws.Cells[i, j].Value2);
+                            //file_writer.Close();
+                            richTextBoxArray.Text += Convert.ToString(ws.Cells[i, j].Value2);
+                        }
+                        else
+                        {
+                            //richTextBox1.Text += ", " + ws.Cells[i, j].Value2;
+                            //file_writer = new StreamWriter(address, true);
+                            //file_writer.Write(", " + ws.Cells[i, j].Value2);
+                            //file_writer.Close();
+                            richTextBoxArray.Text += ", " + Convert.ToString(ws.Cells[i, j].Value2);
+                        }
+                    }
+                    else if (ws.Cells[1, j + 1].Value2 != null)
+                    {
+                        //richTextBox1.Text += " };\n";
+                        //file_writer = new StreamWriter(address, true);
+                        //file_writer.Write(" };\n");
+                        //file_writer.Close();
+                        richTextBoxArray.Text += " };\n";
+                        file_writer = new StreamWriter(address, true);
+                        file_writer.Write("static array<double, 1>^" + Convert.ToString(arrayName) + " = gcnew array <double>(" + (i-2) + ") " + richTextBoxArray.Text);
+                        file_writer.Close();
+                        i = 10000;
+                    }
+                    else
+                    {
+                        //richTextBox1.Text += " };\n";
+                        //file_writer = new StreamWriter(address, true);
+                        //file_writer.Write(" };\n");
+                        //file_writer.Close();
+                        richTextBoxArray.Text += " };\n";
+                        file_writer = new StreamWriter(address, true);
+                        file_writer.Write("static array<double, 1>^" + Convert.ToString(arrayName) + " = gcnew array <double>(" + (i-2) + ") " + richTextBoxArray.Text);
+                        file_writer.Close();
+                        labelConversionCompleted.Text = "Conversion Completed";
+                        labelConversionCompleted.Location = new System.Drawing.Point(29, 93);
+                        labelConversionCompleted.Visible = true;
+                        i = 10000;
+                        j = 10000;
+                    }
+                }
+            }
+            Excel("0", 0);
+        }
+        //
+        //
+        //
+        void Excel(string path, int Sheet)
+        {
+            if (Sheet == 0)
+            {
+                excel.Workbooks.Close();
+            }
+            else
+            {
+                this.path = path;
+                wb = excel.Workbooks.Open(path);
+                ws = wb.Worksheets[Sheet];
+            }
+        }
+
+        private void buttonMinimized_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+            //System.Windows.Forms.Application.ExitThread();
+            //this.Close();
+        }
+        //
+        //
+        //
+        private bool enableMoving = false;
+        private System.Drawing.Point initialClickedPoint;
+        private void panelAbove_MouseDown(object sender, MouseEventArgs e)
+        {
+            enableMoving = true;
+            initialClickedPoint = e.Location;
+        }
+        //
+        //
+        //
+        private void panelAbove_MouseUp(object sender, MouseEventArgs e)
+        {
+            enableMoving = false;
+        }
+        //
+        //
+        //
+        private void panelAbove_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(enableMoving)
+            {
+                this.Location = new System.Drawing.Point(e.X + this.Left - initialClickedPoint.X, e.Y + this.Top - initialClickedPoint.Y);
+            }
+        }
+
+        private void labelTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            enableMoving = true;
+            initialClickedPoint = e.Location;
+        }
+
+        private void labelTitle_MouseUp(object sender, MouseEventArgs e)
+        {
+            enableMoving = false;
+        }
+
+        private void labelTitle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (enableMoving)
+            {
+                this.Location = new System.Drawing.Point(e.X + this.Left - initialClickedPoint.X, e.Y + this.Top - initialClickedPoint.Y);
+            }
+        }
+        private void buttonCSharp_Click(object sender, EventArgs e)
+        {
+            if(cSharp)
+            {
+                cSharpWrite = true;
+                cppWrite = false;
+                cpp = true;
+                cSharp = false;
+                buttonCSharp.ForeColor = Color.DarkSeaGreen;
+                buttonCpp.ForeColor = Color.LightGray;
+                buttonExcel2Text.ForeColor = Color.LightGray;
+                labelConversionCompleted.Visible = false;
+                excelToArray = true;
+
+            }            
+        }
+
+        private void buttonCpp_Click(object sender, EventArgs e)
+        {
+            if (cpp)
+            {
+                cppWrite = true;
+                cSharpWrite = false;                
+                cSharp = true;
+                cpp = false;
+                buttonCpp.ForeColor = Color.DarkSeaGreen;
+                buttonExcel2Text.ForeColor = Color.LightGray;
+                buttonCSharp.ForeColor = Color.LightGray;
+                labelConversionCompleted.Visible = false;
+                excelToArray = true;
+            }
+        }
+    }
+}
